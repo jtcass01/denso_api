d05e339694cf9ed170608e20ff14480739e62b10
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-12-12 21:27:04 -0500

    Attempting to convert ROS tutorial message examples into something more functional for a api server

--- src/ngs_ros/src/ngs_denso_rc8_api/scripts/rc8_api_server.py
+++ src/ngs_ros/src/ngs_denso_rc8_api/scripts/rc8_api_server.py
@@ -7,23 +7,25 @@ from SocketClient import SocketClient
 
 socket_client = SocketClient()
 
-def issue_command_to_rc8(command):
-    rospy.loginfo("Issuing command to Rc8: " + command.data)
-    socket_client.send_message(command.data + '\r')
 
 class API_Server(object):
     def __init__(self):
         self.api = NGS_RC8_API()
+        self.socket_client = SocketClient()
+#        socket_client.prompt_for_connection()
 
     def start_server(self):
         rospy.init_node('ngs_rc8_api_server', anonymous=True)
 
-        rospy.Subscriber("rc8_command", String, issue_command_to_rc8)
+        rospy.Subscriber("rc8_command", String, self.issue_command_to_rc8)
 
         # spin() simply keeps python from exiting until this node is stopped
         rospy.spin()
 
+    def issue_command_to_rc8(self, command):
+        rospy.loginfo("Issuing command to Rc8: " + command.data)
+#        socket_client.send_message(command.data + '\r')
+
 if __name__ == '__main__':
     server = API_Server()
-#    socket_client.prompt_for_connection()
     server.start_server()

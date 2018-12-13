00a082640f088a7cf5abc64bbf14fb7afd9525c5
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-12-12 21:12:59 -0500

    Attempting to convert ROS tutorial message examples into something more functional for a api server

--- src/ngs_ros/src/ngs_denso_rc8_api/scripts/rc8_api_server.py
+++ src/ngs_ros/src/ngs_denso_rc8_api/scripts/rc8_api_server.py
@@ -2,21 +2,29 @@
 import rospy
 from std_msgs.msg import String
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+from NGS_RC8_API import NGS_RC8_API
+from SocketClient import SocketClient
 
-def listener():
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+class API_Server(object):
+    def __init__(self):
+        self.api = NGS_RC8_API()
 
-    rospy.Subscriber("chatter", String, callback)
+    def issue_command_to_rc8(self, command):
+        rospy.loginfo("Issuing command to Rc8: ", command.data)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    def start_server(self):
+        # In ROS, nodes are uniquely named. If two nodes with the same
+        # name are launched, the previous one is kicked off. The
+        # anonymous=True flag means that rospy will choose a unique
+        # name for our 'listener' node so that multiple listeners can
+        # run simultaneously.
+        rospy.init_node('ngs_rc8_api_server', anonymous=True)
+
+        rospy.Subscriber("rc8_command", String, self.issue_command_to_rc8)
+
+        # spin() simply keeps python from exiting until this node is stopped
+        rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    server = API_Server()
+    server.start_server()
--- src/ngs_ros/src/ngs_denso_rc8_api/scripts/test_talker.py
+++ src/ngs_ros/src/ngs_denso_rc8_api/scripts/test_talker.py
@@ -5,8 +5,8 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    pub = rospy.Publisher('rc8_command', String, queue_size=10)
+    rospy.init_node('test_talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
